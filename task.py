2261e472f97032b0366b66efe2f260bb5417cd6f
paspekov34 <nikpaspekov17@mail.ru>
2024-10-27 00:53:33 +0300

    Add files via upload

--- task.py
+++ task.py
@@ -1,28 +1,20 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix='/task', tags=['task'])
-
-
-@router.get('/')
-async def all_tasks():
-    pass
+from app.backend.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from app.models import *
 
 
-@router.get('/task_id')
-async def task_by_id():
-    pass
+class Task(Base):
+    __tablename__ = 'tasks'
+    __table_args__ = {'keep_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False, index=True)
+    slug = Column(String, unique=True, index=True)
+    user = relationship("User", back_populates='task')
 
-
-@router.post('/create')
-async def create_task():
-    pass
-
-
-@router.put('/update')
-async def update_task():
-    pass
-
-
-@router.delete('/delete')
-async def delete_task():
-    pass
+from sqlalchemy.schema import CreateTable
+print(CreateTable(Task.__table__))
--- users.py
+++ users.py
@@ -1,28 +1,20 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix='/user', tags=['user'])
-
-
-@router.get('/')
-async def all_users():
-    pass
+from app.backend.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
+from app.models import *
 
 
-@router.get('/user_id')
-async def user_id():
-    pass
 
+class User(Base):
+    __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    tasks = relationship("Task", back_populates='user')
 
-@router.post('/create')
-async def create_user():
-    pass
-
-
-@router.put('/update')
-async def update_user():
-    pass
-
-
-@router.delete('/delete')
-async def delete_user():
-    pass
+from sqlalchemy.schema import CreateTable
+print(CreateTable(User.__table__))
